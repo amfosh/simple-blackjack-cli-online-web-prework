e09f9440e8739ebec541f17c5864a4a8d8f89a0e
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-29 23:12:14 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -3,16 +3,16 @@ def welcome
 end
 
 def deal_card
- rand(1..11)
+ card = rand(1..11)
+end
+
+def display_card_total
+  card_total = number
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   puts "Type 'h' to hit or 's' to stay"
-end
-
-def display_card_total(number)
-  card_total = number
-  puts "Your cards add up to #{card_total}"
 end
 
 def get_user_input
