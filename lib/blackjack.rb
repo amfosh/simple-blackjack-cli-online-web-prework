23edd7d3f6ba5cb5aad8ce845e5fd8de54363581
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-29 23:04:37 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,12 +25,11 @@ def end_game
 end
 
 def initial_round
-  sum = deal_card + deal_card
-  display_card_total(sum)
-  return sum
+  display_card_total = deal_card + deal_card
+  return display_card_total
 end
 
-def hit?(card_total)
+def hit?(number)
   prompt_user
   get_user_input
   if user_input == "h"
