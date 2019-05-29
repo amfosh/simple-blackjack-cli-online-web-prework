36b79798241eb02bdbad58a6e5725ab74dc70382
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-29 23:37:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,9 +51,10 @@ end
 def runner
   welcome 
   card_total = initial_round
-  until card_total > 21
+    until card_total > 21
   card_total = hit?(card_total)
   display_card_total(card_total)
   end_game(card_total)
+end
 end
 end
